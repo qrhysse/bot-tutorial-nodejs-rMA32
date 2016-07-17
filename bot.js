41e7d6d329b3ff9e31f75f7fdc478659db7b414f
Augustus Vieweg <awvieweg@gmail.com>
2016-07-17 16:53:08 -0400

    scoreboard cleanup

--- bot.js
+++ bot.js
@@ -1,68 +1,8 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+var scoreboard = require('./sb.js');
 
 var botID = process.env.BOT_ID;
-
-// Non-breaking spaces save the day
-function spaceCalc(sbheader, header, obj) {
-  var num = sbheader.length - (String(obj).length + header.length);
-  return Array(num+1).join('\u00A0');
-}
-
-//pointless test change
-
-function getScoreboard(lols, darns, date) {
-    console.log('getScoreboard called');
-    var scoreboardHead = "-------------SCOREBOARD-------------";
-    var totalLols = "TOTAL LOLS:";
-    var totalDarns = "TOTAL DARNS:";
-    var lolspaces = spaceCalc(scoreboardHead, totalLols, lols);
-    var darnspaces = spaceCalc(scoreboardHead, totalDarns, darns);
-    var lolLine = totalLols + lolspaces + lols;
-    var darnLine = totalDarns + darnspaces + darns;
-    var scoreboard = "-------------SCOREBOARD-------------\n\n" + lolLine + "\n" + darnLine + "\n\n---------Since "+date+"---------";
-    console.log('Scoreboard is: ', scoreboard);
-    return scoreboard;
-}
-
-function scoreboard(forParse, resp) {
-  if( forParse.group_id !== '23073839' ) {
-    var lolTrigger = /(lol|\blol)/ig;
-    var darnTrigger = /(darn|\bdarn)/ig;
-    var botRegexScoreboard = /\/scoreboard/i;
-    var request = require('request');
-    var jsonObj, lolCount, darnCount, currentDate, returnval = 0;
-    var count = 0;
-
-    request('https://api.myjson.com/bins/4xupz', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var jsonObj = JSON.parse(body);
-        var lolCount = jsonObj.lols;
-        var darnCount = jsonObj.darns;
-        var currentDate = jsonObj.date;
-        if( forParse.text && botRegexScoreboard.test(forParse.text)) {
-          returnval = getScoreboard(lolCount, darnCount, currentDate);
-          resp.res.writeHead(200);
-          postMessage(returnval, false);
-          resp.res.end();
-        } else {
-          if( lolTrigger.test(forParse.text)) {
-            count = (forParse.text.match(lolTrigger) || []).length;
-            lolCount += count;
-          }
-          if( darnTrigger.test(forParse.text)) {
-            count = (forParse.text.match(darnTrigger) || []).length;
-            darnCount += count;
-          }
-          request({ url: 'https://api.myjson.com/bins/4xupz', method: 'PUT', json: {lols: lolCount, darns: darnCount, date: currentDate}});
-          returnval = 0;
-        }
-      } else {
-        returnval = 0;
-      }
-    });
-  }
-}
 
 function respond() { 
   var request = JSON.parse(this.req.chunks[0]);
@@ -94,7 +34,7 @@ function respond() {
       this.res.end();
     }
 
-    scoreboard( request, this );
+    scoreboard.scoreboard( request, this );
     
     if(request.text && botRegexBio.test(request.text)) {
       var link = request.text;
---
+++ sb.js
@@ -0,0 +1,66 @@
+// Non-breaking spaces save the day
+var spaceCalc = function (sbheader, header, obj) {
+  var num = sbheader.length - (String(obj).length + header.length);
+  return Array(num+1).join('\u00A0');
+}
+
+//pointless test change
+
+var getScoreboard = function (lols, darns, date) {
+    console.log('getScoreboard called');
+    var scoreboardHead = "-------------SCOREBOARD-------------";
+    var totalLols = "TOTAL LOLS:";
+    var totalDarns = "TOTAL DARNS:";
+    var lolspaces = spaceCalc(scoreboardHead, totalLols, lols);
+    var darnspaces = spaceCalc(scoreboardHead, totalDarns, darns);
+    var lolLine = totalLols + lolspaces + lols;
+    var darnLine = totalDarns + darnspaces + darns;
+    var scoreboard = "-------------SCOREBOARD-------------\n\n" + lolLine + "\n" + darnLine + "\n\n---------Since "+date+"---------";
+    console.log('Scoreboard is: ', scoreboard);
+    return scoreboard;
+}
+
+var scoreboard = function (forParse, resp) {
+  if( forParse.group_id !== '23073839' ) {
+    var lolTrigger = /(lol|\blol)/ig;
+    var darnTrigger = /(darn|\bdarn)/ig;
+    var botRegexScoreboard = /\/scoreboard/i;
+    var request = require('request');
+    var jsonObj, lolCount, darnCount, currentDate, returnval = 0;
+    var count = 0;
+
+    request('https://api.myjson.com/bins/4xupz', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var jsonObj = JSON.parse(body);
+        var lolCount = jsonObj.lols;
+        var darnCount = jsonObj.darns;
+        var currentDate = jsonObj.date;
+        if( forParse.text && botRegexScoreboard.test(forParse.text)) {
+          returnval = getScoreboard(lolCount, darnCount, currentDate);
+          resp.res.writeHead(200);
+          postMessage(returnval, false);
+          resp.res.end();
+        } else {
+          if( lolTrigger.test(forParse.text)) {
+            count = (forParse.text.match(lolTrigger) || []).length;
+            lolCount += count;
+          }
+          if( darnTrigger.test(forParse.text)) {
+            count = (forParse.text.match(darnTrigger) || []).length;
+            darnCount += count;
+          }
+          request({ url: 'https://api.myjson.com/bins/4xupz', method: 'PUT', json: {lols: lolCount, darns: darnCount, date: currentDate}});
+          returnval = 0;
+        }
+      } else {
+        returnval = 0;
+      }
+    });
+  }
+}
+
+module.exports = {
+  scoreboard: scoreboard,
+  getScoreboard: getScoreboard,
+  spaceCalc: spaceCalc
+}
