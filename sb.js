169f127e69d5b2020b87a53c64e6024341df5d31
Augustus Vieweg <awvieweg@gmail.com>
2016-07-17 16:56:45 -0400

    reupdate for safety

--- sb.js
+++ sb.js
@@ -21,44 +21,42 @@ var getScoreboard = function (lols, darns, date) {
 }
 
 var scoreboard = function (forParse, resp) {
-  if( forParse.group_id !== '23073839' ) {
-    var lolTrigger = /(lol|\blol)/ig;
-    var darnTrigger = /(darn|\bdarn)/ig;
-    var botRegexScoreboard = /\/scoreboard/i;
-    var request = require('request');
-    var jsonObj, lolCount, darnCount, currentDate, returnval = 0;
-    var count = 0;
+  var lolTrigger = /(lol|\blol)/ig;
+  var darnTrigger = /(darn|\bdarn)/ig;
+  var botRegexScoreboard = /\/scoreboard/i;
+  var request = require('request');
+  var jsonObj, lolCount, darnCount, currentDate, returnval = 0;
+  var count = 0;
 
-    request('https://api.myjson.com/bins/4xupz', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var jsonObj = JSON.parse(body);
-        var lolCount = jsonObj.lols;
-        var darnCount = jsonObj.darns;
-        var currentDate = jsonObj.date;
-        if( forParse.text && botRegexScoreboard.test(forParse.text)) {
-          returnval = getScoreboard(lolCount, darnCount, currentDate);
-          resp.res.writeHead(200);
-          postMessage(returnval, false);
-          resp.res.end();
-        } else {
-          if( forParse.group_id !== '23073839' ) {
-            if( lolTrigger.test(forParse.text)) {
-              count = (forParse.text.match(lolTrigger) || []).length;
-              lolCount += count;
-            }
-            if( darnTrigger.test(forParse.text)) {
-              count = (forParse.text.match(darnTrigger) || []).length;
-              darnCount += count;
-            }
-          }
-          request({ url: 'https://api.myjson.com/bins/4xupz', method: 'PUT', json: {lols: lolCount, darns: darnCount, date: currentDate}});
-          returnval = 0;
-        }
+  request('https://api.myjson.com/bins/4xupz', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var jsonObj = JSON.parse(body);
+      var lolCount = jsonObj.lols;
+      var darnCount = jsonObj.darns;
+      var currentDate = jsonObj.date;
+      if( forParse.text && botRegexScoreboard.test(forParse.text)) {
+        returnval = getScoreboard(lolCount, darnCount, currentDate);
+        resp.res.writeHead(200);
+        postMessage(returnval, false);
+        resp.res.end();
       } else {
+        if( forParse.group_id !== '23073839' ) {
+          if( lolTrigger.test(forParse.text)) {
+            count = (forParse.text.match(lolTrigger) || []).length;
+            lolCount += count;
+          }
+          if( darnTrigger.test(forParse.text)) {
+            count = (forParse.text.match(darnTrigger) || []).length;
+            darnCount += count;
+          }
+        }
+        request({ url: 'https://api.myjson.com/bins/4xupz', method: 'PUT', json: {lols: lolCount, darns: darnCount, date: currentDate}});
         returnval = 0;
       }
-    });
-  }
+    } else {
+      returnval = 0;
+    }
+  });
 }
 
 module.exports = {
